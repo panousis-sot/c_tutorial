0bc8fb9bc93ffc1d898b313b72c4a8026ba7b468
panousis-sot <sotiriospanousis@gmail.com>
2026-05-03 00:28:34 +0200

    if else course

---
+++ if_else.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int age = 30;
+
+    if (age >= 18) {
+        printf("Allowed\n");
+    }
+    else {
+        printf("Not allowed\n");
+    }
+
+     return 0;
+}
